8ca6d2c11de6ee71fa99ac18e28f3a0985014c9b
Kit Cross <kit.cross@fusion.net>
2014-09-11 17:15:06 -0400

    Style changes

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,3 +1,17 @@
-console.log('app ready');
+require('jquery');
+var Spreadsheets = require('google-spreadsheets');
 
-console.log();
+
+
+
+Spreadsheets({
+    key: "1ksjdv6hT3rZh9tYkOo8MtwVT5_0vq8AFPMk8hz1xKmc"
+}, function(err, spreadsheet) {
+
+    spreadsheet.worksheets[0].cells({
+        range: "R1C1:R5C5"
+    }, function(err, cells) {
+        // Cells will contain a 2 dimensional array with all cell data in the
+        // range requested.
+    });
+});
