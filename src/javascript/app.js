0f44a2b48f71a3c9a15b0a7006935ba9d93fc41a
Kit Cross <kit.cross@fusion.net>
2014-09-12 16:14:59 -0400

    Add select menus

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,17 +1,16 @@
 require('jquery');
+
+
 var Spreadsheets = require('google-spreadsheets');
 
+// Spreadsheets({
+//     key: "1ksjdv6hT3rZh9tYkOo8MtwVT5_0vq8AFPMk8hz1xKmc"
+// }, function(err, spreadsheet) {
 
-
-
-Spreadsheets({
-    key: "1ksjdv6hT3rZh9tYkOo8MtwVT5_0vq8AFPMk8hz1xKmc"
-}, function(err, spreadsheet) {
-
-    spreadsheet.worksheets[0].cells({
-        range: "R1C1:R5C5"
-    }, function(err, cells) {
-        // Cells will contain a 2 dimensional array with all cell data in the
-        // range requested.
-    });
-});
+//     spreadsheet.worksheets[0].cells({
+//         range: "R1C1:R5C5"
+//     }, function(err, cells) {
+//         // Cells will contain a 2 dimensional array with all cell data in the
+//         // range requested.
+//     });
+// });
