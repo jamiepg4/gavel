8312b16dd8738c775711e21eadd6f8214445de35
Kit Cross <kit.cross@fusion.net>
2014-09-16 12:52:36 -0400

    Refresh Backbone style

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,24 +1,32 @@
-require('jquery');
+var $ = require('jquery');
 var Backbone = require('backbone');
+
+Backbone.$ = $;
 
 var app = app || {};
 
-console.log('something');
+$(function () {
 
-app.AppView = Backbone.View.extend({
+    'use strict';
 
-    el: '#gavel',
-    events: {
-        'click #button': 'clickButton'
-    },
+    var AppView = Backbone.View.extend({
 
-    initialize: function () {
-        console.log('initialise gavel');
-    },
+        el: '#gavel',
+        events: {
+            'click #button': 'clickButton'
+        },
 
-    click: function () {
-        console.log('clicked button');
-    }
+        initialize: function () {
+            console.log('initialise gavel');
+        },
 
+        click: function () {
+            console.log('clicked button');
+        }
+
+
+    });
+
+    var appView = new AppView({el: $('#gavel')});
 
 });
