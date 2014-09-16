cca3803802380bf801d05997d96e69a7b9052d2e
Kit Cross <kit.cross@fusion.net>
2014-09-16 11:57:30 -0400

    Add backbone to list of dependencies

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,16 +1,24 @@
 require('jquery');
+var Backbone = require('backbone');
+
+var app = app || {};
+
+console.log('something');
+
+app.AppView = Backbone.View.extend({
+
+    el: '#gavel',
+    events: {
+        'click #button': 'clickButton'
+    },
+
+    initialize: function () {
+        console.log('initialise gavel');
+    },
+
+    click: function () {
+        console.log('clicked button');
+    }
 
 
-var Spreadsheets = require('google-spreadsheets');
-
-// Spreadsheets({
-//     key: "1ksjdv6hT3rZh9tYkOo8MtwVT5_0vq8AFPMk8hz1xKmc"
-// }, function(err, spreadsheet) {
-
-//     spreadsheet.worksheets[0].cells({
-//         range: "R1C1:R5C5"
-//     }, function(err, cells) {
-//         // Cells will contain a 2 dimensional array with all cell data in the
-//         // range requested.
-//     });
-// });
+});
